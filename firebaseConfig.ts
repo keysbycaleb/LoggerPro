0e033ef6bf3de07724d89ed7b39b0d4e18f107fc
keysbycaleb <keysbycaleb@gmail.com>
2025-09-05 03:42:00 +0000

    Add Firebase configuration and install SDK

---
+++ firebaseConfig.ts
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyYOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
